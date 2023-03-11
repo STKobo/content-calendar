4483c01ba08dd4e7974bc1d5a3a3963477151844
Stephane_tcha <stephanetcha@gmail.com>
2023-03-11 22:43:40 +0100

    view the Bean we created

--- src/main/java/com/hmoo/contentcalendar/Application.java
+++ src/main/java/com/hmoo/contentcalendar/Application.java
@@ -5,13 +5,15 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		ConfigurableApplicationContext context =  SpringApplication.run(Application.class, args);
+		RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
+		System.out.println(restTemplate);
 	}
 
 }
