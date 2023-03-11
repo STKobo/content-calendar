692a5d7729c1135834298b6e90a01b68bbf0fbf4
Stephane_tcha <stephanetcha@gmail.com>
2023-03-11 22:15:40 +0100

    check beans

--- src/main/java/com/hmoo/contentcalendar/Application.java
+++ src/main/java/com/hmoo/contentcalendar/Application.java
@@ -1,13 +1,17 @@
 package com.hmoo.contentcalendar;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
