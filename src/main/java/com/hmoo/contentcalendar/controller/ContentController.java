53d742e51e7b42ae0d6b9eabe1defc5233d67aad
Stephane_tcha <stephanetcha@gmail.com>
2023-03-12 19:42:31 +0100

    dependency injection, now we have a instance of ContentCollectionRepository

--- src/main/java/com/hmoo/contentcalendar/controller/ContentController.java
+++ src/main/java/com/hmoo/contentcalendar/controller/ContentController.java
@@ -1,10 +1,19 @@
 package com.hmoo.contentcalendar.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hmoo.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
     
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository){
+        this.repository = repository;
+    }
+
 }
