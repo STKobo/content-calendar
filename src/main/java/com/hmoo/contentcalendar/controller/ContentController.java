9e78a3e66325be90eb6a34e381b0bdb5c11ea624
Stephane_tcha <stephanetcha@gmail.com>
2023-03-12 19:52:34 +0100

    create a method wich to the outside, @GetMapping

--- src/main/java/com/hmoo/contentcalendar/controller/ContentController.java
+++ src/main/java/com/hmoo/contentcalendar/controller/ContentController.java
@@ -1,9 +1,13 @@
 package com.hmoo.contentcalendar.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hmoo.contentcalendar.model.Content;
 import com.hmoo.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
@@ -14,6 +18,11 @@ public class ContentController {
 
     public ContentController(ContentCollectionRepository repository){
         this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
     }
 
 }
