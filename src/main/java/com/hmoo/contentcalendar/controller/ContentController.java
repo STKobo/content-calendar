b16b24612679fd3e73346e7abdba8dd7e1218a1f
Stephane_tcha <stephanetcha@gmail.com>
2023-03-13 17:04:04 +0100

    @RequestBody indicating that the method parameter should be bound to the body of the web request

--- src/main/java/com/hmoo/contentcalendar/controller/ContentController.java
+++ src/main/java/com/hmoo/contentcalendar/controller/ContentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -36,7 +37,7 @@ public class ContentController {
     }
 
     @PostMapping("")
-    public void create(Content content){
+    public void create(@RequestBody Content content){
         repository.save(content);
     }
 
